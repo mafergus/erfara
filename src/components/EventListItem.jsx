6002ca5517eba40225dbd34ef5d87beb511fcd4c
Matt <matthew.n.ferguson@gmail.com>
2017-02-05 04:28:24 -0800

    Stuff

--- src/components/EventListItem.jsx
+++ src/components/EventListItem.jsx
@@ -1,11 +1,27 @@
 import React, { PropTypes } from "react";
 import autoBind from "react-autobind"
+import { connect } from "react-redux";
 import { Link } from "react-router";
 import { GridTile } from 'material-ui/GridList';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import IconButton from 'material-ui/IconButton';
+import { addEventPhoto } from "../actions/eventActions";
+import store from "../store/store";
 
-export default class EventListItem extends React.Component {
+function mapStateToProps(state, props) {
+  return {
+    event: state.events.get(props.eventUid),
+    photoURL: event && event.photoURL,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+
+  };
+}
+
+export class EventListItem extends React.Component {
 
   static PropTypes = {
     event: PropTypes.object.isRequired,
@@ -18,27 +34,18 @@ export default class EventListItem extends React.Component {
 	}
 
   componentWillMount() {
-    this.fetchPhoto();
+    if (this.props.event) {
+      this.fetchPhoto();
+    }
   }
 
   fetchPhoto() {
-    debugger;
-    const id = event.split(/:(.+)/)[1];
-    fetch(`https://pixabay.com/api/?key=4423887-ab96e540ffbe404d644032133&id=${id}&pretty=true`).then(function(response) {
-      if (response.ok) {
-        return response.json();
-      }
-    }).then(function(json) {
-      debugger;
-      if (json && json.hits && json.hits.length > 0) {
-      }
-    }).catch(function(error) {
-      console.log("UH OH SHIT FUCKED UP: ", error);
-    });
+    const id = this.props.event.photo.split(/:(.+)/)[1];
+    store.dispatch(addEventPhoto(this.props.eventUid, id));
   }
 
 	render() {
-    const { event, eventUid } = this.props;
+    const { event, eventUid, photoURL } = this.props;
 		return <Link to={`/event/${eventUid}`}>
       <GridTile
         key={eventUid}
@@ -46,8 +53,10 @@ export default class EventListItem extends React.Component {
         subtitle={<span>by <b>{event.description}</b></span>}
         actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
       >
-        <img src={this.photo || "http://www.tuscanysportholidays.com/media/k2/items/cache/1c0ae2205709722b62e843abc0471a55_XL.jpg"} />
+        <img src={photoURL} />
       </GridTile>
      </Link>;
 	}
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventListItem);
