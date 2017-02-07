527fc73fee82293e6a61e41eacbf871006b318f2
matt-thinair <matt@thinair.com>
2017-02-06 21:56:58 -0800

    Upload images to firebase

--- src/components/EventListItem.jsx
+++ src/components/EventListItem.jsx
@@ -5,19 +5,11 @@ import { Link } from "react-router";
 import { GridTile } from 'material-ui/GridList';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import IconButton from 'material-ui/IconButton';
-import { addEventPhoto } from "../actions/eventActions";
 import store from "../store/store";
 
 function mapStateToProps(state, props) {
   return {
     event: state.events.get(props.eventUid),
-    photoURL: event && event.photoURL,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-
   };
 }
 
@@ -33,19 +25,8 @@ export class EventListItem extends React.Component {
 		autoBind(this);
 	}
 
-  componentWillMount() {
-    if (this.props.event) {
-      this.fetchPhoto();
-    }
-  }
-
-  fetchPhoto() {
-    const id = this.props.event.photo.split(/:(.+)/)[1];
-    store.dispatch(addEventPhoto(this.props.eventUid, id));
-  }
-
 	render() {
-    const { event, eventUid, photoURL } = this.props;
+    const { event, eventUid } = this.props;
 		return <Link to={`/event/${eventUid}`}>
       <GridTile
         key={eventUid}
@@ -53,10 +34,10 @@ export class EventListItem extends React.Component {
         subtitle={<span>by <b>{event.description}</b></span>}
         actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
       >
-        <img src={photoURL} />
+        <img src={event.photo} />
       </GridTile>
      </Link>;
 	}
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(EventListItem);
+export default connect(mapStateToProps)(EventListItem);
