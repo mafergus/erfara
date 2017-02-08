371e2492c64a3549a97a524dd3499c458ced41d6
matt-thinair <matt@thinair.com>
2017-02-07 22:51:17 -0800

    Make conversation list resizable

--- src/components/Messaging/MessagingPage.jsx
+++ src/components/Messaging/MessagingPage.jsx
@@ -1,10 +1,12 @@
 import React, { PropTypes } from "react";
 import autoBind from "react-autobind";
 import { connect } from "react-redux";
+import Draggable from 'react-draggable';
 import ConversationList from "./ConversationList";
 import MessagesWindow from "./MessagesWindow";
 import store from "../../store/store";
 import { addMessage } from "../../actions/messageActions";
+import Resizable from "react-resizable-box";
 
 function mapStateToProps(state, props) {
   return {
@@ -46,8 +48,25 @@ export class MessagingPage extends React.Component {
 
   render() {
     return <div style={{ width: "100%", height: "100%", position: "fixed", maxWidth: "1440px", top: "64px", left: "0", display: "flex" }}>
-      <ConversationList conversations={this.props.conversations} onConversationSelected={this.onConversationSelected} style={{ display: "inline-block", height: "100%", marginTop: "0px", width: "30%" }}/>
-      <MessagesWindow conversation={this.props.conversations.get(this.state.conversationId)} onSendMessage={this.sendMessage} style={{ width: "70%", height: "100%", display: "inline-block" }}/>
+      <Resizable
+        customClass="item"
+        width="20%"
+        minWidth={250}
+        maxWidth={600}
+        isResizable={{top:false, right:true, bottom:false, left:false, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false}}
+        height="100%"
+      >
+        <ConversationList 
+          conversations={this.props.conversations}
+          onConversationSelected={this.onConversationSelected}
+          style={{ display: "inline-block", height: "100%", width: "100%", marginTop: "0px" }}
+        />
+      </Resizable>
+      <MessagesWindow 
+        conversation={this.props.conversations.get(this.state.conversationId)}
+        onSendMessage={this.sendMessage}
+        style={{ width: "70%", height: "100%", display: "inline-block" }}
+      />
     </div>
   }
 
