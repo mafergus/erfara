04f474d92d710e754a6b0ab54fd06e61dac2aa66
matt-thinair <matt@thinair.com>
2017-02-08 00:56:10 -0800

    Start of feed

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -4,11 +4,13 @@ import { messagesReducer } from "./messagesReducer";
 import { authedUserReducer } from "./authedUserReducer";
 import { conversationsReducer } from "./conversationsReducer";
 import { usersReducer } from "./usersReducer";
+import { feedReducer } from "./feedReducer";
 
 const rootReducer = combineReducers({
   authedUser: authedUserReducer,
   conversations: conversationsReducer,
   events: eventsReducer,
+  feed: feedReducer,
   messages: messagesReducer,
   users: usersReducer,
 });
