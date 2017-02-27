d27cdc9f7fc280572999d5000cb01cef09d09d23
Cihan <cihannisan@gmail.com>
2017-02-26 16:28:37 -0800

    Re-styled Create evenet modal

--- src/components/Modals/CreateEventModal.jsx
+++ src/components/Modals/CreateEventModal.jsx
@@ -1,8 +1,6 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
-import { darkBlack } from 'material-ui/styles/colors';
 import autoBind from 'react-autobind';
 import TextField from "material-ui/TextField";
 import DatePicker from 'material-ui/DatePicker';
@@ -10,6 +8,7 @@ import TimePicker from 'material-ui/TimePicker';
 import store from "store/store";
 import { addEvent } from "actions/eventActions";
 import { getPhoto, uploadFile } from "utils/Api";
+import "components/Modals/CreateEventModal.css"
 
 const PLACEHOLDER_PHOTO = "http://files.parsetfss.com/a5e80e30-a275-49f2-989e-e218e12017db/tfss-02ed6157-7aa6-4ffa-b530-16f711fb8f59-muir-woods.jpg";
 
@@ -75,49 +74,151 @@ export class CreateEventModal extends React.Component {
   }
 
   render() {
-    const actions = [
-      <FlatButton
-        label="Cancel"
-        primary={true}
-        onTouchTap={this.props.onRequestClose}
-      />,
-      <FlatButton
-        label="Submit"
-        primary={true}
-        keyboardFocused={true}
-        onTouchTap={this.addNewEvent}
-      />,
-    ];
+    const style = {
+      hintStyle: {
+        color: "#BDBDBD", 
+        fontFamily: ".AppleSystemUIFont",
+        fontSize: "12px"
+      },
+      descriptionHintStyle: {
+        color: "#BDBDBD", 
+        fontFamily: ".AppleSystemUIFont",
+        fontSize: "12px",
+        top: "12px"
+      },
+      textFieldStyle: {
+        paddingLeft:"10px",
+        width: "inherit",
+        fontSize:"12px"
+      }
+    }
 
     return (
+      <div className="popup-dialog">
         <Dialog
-          actions={actions}
-          contentStyle={{textAlign: "center", width: "60%", height: "500px"}}
-          title={"Create Event"}
-          titleStyle={{ fontSize: "1.1em", textAlign: "left", padding: "12px 0px 12px 25px", color: darkBlack }}
+          contentStyle={{width: "55%", minWidth:"660px"}}
           modal={false}
           onRequestClose={this.props.onRequestClose}
           open={this.props.isOpen}>
-          <div style={{ display: "flex", alignItems: "flex-start", flexDirection: "column" }}>
-            <TextField
-              hintText="Event Name"
-              floatingLabelText="Event Name"
-              onChange={(event, value) => { this.name = value; }}
-            />
-            <TextField
-              hintText="Description"
-              floatingLabelText="Description"
-              onChange={(event, value) => { this.description = value; }}
-            />
-            <DatePicker hintText="Date" onChange={this.dateChange} />
-            <TimePicker hintText="12hr Format" onChange={this.timeChange} />
-            <TextField
-              hintText="Location"
-              floatingLabelText="Location"
-              onChange={(event, value) => { this.locationString = value; }}
-            />
+          <div>
+            <a href="#" onClick={this.props.onRequestClose} className="close-btn">&times;</a>
+            <h3>Create an Event</h3>
+            <div className="row-1">
+              <div className="event-title">
+                <div className="label">
+                  <label>Event Title</label>
+                </div>
+                <div className="box">
+                  <TextField
+                    name="title"
+                    hintText="Give your event a short name"
+                    hintStyle={style.hintStyle}
+                    underlineShow={false}
+                    style={style.textFieldStyle}
+                    onChange={(event, value) => { this.name = value; }}
+                  />
+                </div>
+              </div>
+              <div className="event-location">
+              <div className="label">
+                <label>Location</label>
+              </div>
+              <div className="box">
+                <TextField
+                  name="location"
+                  hintText="Add a place or address"
+                  hintStyle={style.hintStyle}
+                  underlineShow={false}
+                  style={style.textFieldStyle}
+                  onChange={(event, value) => { this.locationString = value; }}
+                />
+              </div>
+              </div>
+            </div>
+            <div className="row-2">
+              <div className="event-date">
+                <div className="label">
+                  <label>Date</label>
+                </div>
+                <div className="box box-small">
+                  <DatePicker 
+                    name="date"
+                    hintText="7/23/17"
+                    hintStyle={style.hintStyle}
+                    textFieldStyle={style.textFieldStyle}
+                    underlineShow={false} 
+                    onChange={this.dateChange} />
+                </div>
+              </div>
+              <div className="start-time">
+                <div className="label">
+                  <label>Start Time</label>
+                </div>
+                <div className="box box-small">
+                  <TimePicker 
+                    name="startTime"
+                    hintText="4:00 PM"
+                    hintStyle={style.hintStyle}
+                    textFieldStyle={style.textFieldStyle}
+                    underlineShow={false}  
+                    onChange={this.timeChange} />
+                </div>
+              </div>
+              <div className="end-time">
+                <div className="label">
+                  <label>End Time</label>
+                </div>
+                <div className="box box-small">
+                  <TimePicker 
+                    name="endTime"
+                    hintText="5:00 PM"
+                    hintStyle={style.hintStyle}
+                    textFieldStyle={style.textFieldStyle}
+                    underlineShow={false}  
+                    onChange={this.timeChange} />
+                </div>
+              </div>
+              <div className="event-info">
+                <div className="label">
+                  <label>What should invitees bring?</label>
+                </div>
+                <div className="box">
+                  <TextField
+                    name="info"
+                    hintText="i.e. water bottle, comfortable shoes"
+                    hintStyle={style.hintStyle}
+                    style={style.textFieldStyle}
+                    underlineShow={false}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="row-3">
+              <div className="event-description" >
+                <div className="label">
+                  <label>Event Description</label>
+                </div>
+                <div className="box box-big">
+                  <TextField
+                    name="description"
+                    hintText="Give more detail so people know what your event is about"
+                    hintStyle={style.descriptionHintStyle}
+                    style={style.textFieldStyle}
+                    underlineShow={false}
+                    multiLine={true}
+                    rows={3}
+                    rowsMax={3}
+                    onChange={(event, value) => { this.description = value; }}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="row-4">
+              <button type='submit' className="create-btn" onClick={() => {this.addNewEvent()}}>CREATE</button>
+            </div>
           </div>
         </Dialog>
+      </div>
     );
   }
 }
