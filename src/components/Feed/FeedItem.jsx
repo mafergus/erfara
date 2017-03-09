7d1120bdd4e0507af9a0c13d17f9d68725b09521
mafergus <matthew.n.ferguson@gmail.com>
2017-03-09 01:56:24 -0300

    Event page updates

--- src/components/Feed/FeedItem.jsx
+++ src/components/Feed/FeedItem.jsx
@@ -3,8 +3,9 @@ import { connect } from "react-redux";
 import autoBind from "react-autobind";
 import { bindActionCreators } from "redux";
 import { darkBlack, lightBlack, minBlack } from "material-ui/styles/colors";
+import { darkGray } from "utils/colors";
 import { getUser } from "actions/userActions";
-import { formatDate } from "utils/dateTimeHelpers";
+import Moment from "moment";
 
 function mapStateToProps(state, props) {
   return {
@@ -39,17 +40,20 @@ export class FeedItem extends React.Component {
 
   render() {
     const { feedItem, user } = this.props;
-    const timeStr = formatDate(feedItem.timestamp, false);
-    return <div style={{ display: "flex", padding: "15px 0px 15px 0px", width: "100%" }} className="hover">
-      <div>
-        <img alt="User" style={{ height: "40px", width: "40px", margin: "10px", marginRight: "20px", borderRadius: "50%", verticalAlign: "top" }} src={user.photo}/>
-      </div>
-      <div style={{ height: "100%", flexGrow: "1" }}>
-        <div style={{ marginBottom: "0.6em" }}>
-          <span style={{ color: darkBlack, fontSize: "0.9em", fontWeight: "500" }}>{user.name}</span>
-          <span style={{ float: "right", color: minBlack, fontWeight: "500", fontSize: "0.75em" }}>{timeStr}</span>
+    const moment = Moment(feedItem.timestamp);
+    return <div style={{ padding: "15px 0px 15px 0px", width: "100%" }}>
+      <div style={{ display: "flex" }}>
+        <div style={{ height: "100%" }}>
+          <img alt="User" style={{ height: 50, width: 50, margin: "0 10px 10px 10px", marginRight: "20px", borderRadius: "50%", verticalAlign: "top" }} src={user.photo}/>
         </div>
-        <span style={{ color: lightBlack }}>{feedItem.message}</span>
+        <div style={{ height: "100%", flexGrow: "1" }}>
+          <div style={{ marginBottom: "0.6em" }}>
+            <span style={{ color: darkBlack, fontSize: "0.9em", fontWeight: "500" }}>{user.name}</span>
+            <span style={{ float: "right", color: minBlack, fontWeight: "500", fontSize: "0.75em" }}>{moment.fromNow()}</span>
+          </div>
+          <span style={{ color: lightBlack }}>{feedItem.message}</span>
+          <div style={{ padding: "1.4em 0em", fontSize: "0.9em" }}><span className="hoverable" style={{ color: darkGray }} onClick={() => alert("BOOM")}>Reply</span></div>
+        </div>
       </div>
       <hr/>
     </div>;
