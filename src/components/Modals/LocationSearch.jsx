1cebddba1ef824fcb3c4a703f07c7ca37f9c5aa1
Cihan <cihannisan@gmail.com>
2017-03-10 18:25:26 -0800

    Remove textfield component

--- src/components/Modals/LocationSearch.jsx
+++ src/components/Modals/LocationSearch.jsx
@@ -1,6 +1,5 @@
 import React, { PropTypes } from "react";
 import autoBind from "react-autobind";
-//import TextField from "material-ui/TextField";
 import AutoComplete from 'material-ui/AutoComplete';
 import { addLocationSearchResults } from "actions/locationSearchActions";
 import { autoCompletePlaces } from "utils/Api";
@@ -68,7 +67,6 @@ export class LocationSearch extends React.Component {
           listStyle={{width:'301px' ,marginLeft:"-8px"}}
           underlineShow={false}
           onUpdateInput={(value, dataSource, params) => {this.gotText(value, dataSource, params)}}
-          open={this.state.isListOpen}
         />
       </div>
     );
