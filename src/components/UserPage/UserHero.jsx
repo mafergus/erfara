3ee222bf03caa35c8eda95f7de3eccc10781f9b1
mafergus <matthew.n.ferguson@gmail.com>
2017-03-18 14:14:04 -0700

    User page event list and follow/unfollow

--- src/components/UserPage/UserHero.jsx
+++ src/components/UserPage/UserHero.jsx
@@ -31,15 +31,15 @@ export default class UserHero extends React.Component {
     const followLabel = isFollowing ? "Unfollow" : "Follow";
     return <div style={{ ...HERO_STYLE, backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.7)), url('${user.coverPhoto}')`, borderBottom: "1px solid rgba(0, 0, 0, 0.08)" }} >
       <div style={{ width: "75%", height: "100%", position: "relative", margin: "0 auto" }}>
-        <div style={{ height: 80, position: "absolute", bottom: 10, left: 0, right: 0 }}>
+        <div style={{ height: 80, position: "absolute", bottom: 7, left: 0, right: 0 }}>
           <img alt="User" src={user.photo} style={{ height: 80, width: 80, borderRadius: "50%" }} />
           <div style={{ display: "inline-block", height: 80, paddingLeft: 35, overflow: "hidden" }}>
-            <div style={{ display: "flex", alignItems: "center", height: "100%" }}>
+            <div style={{ display: "flex", alignItems: "flex-end", height: "100%" }}>
               <span style={{ fontSize: "1.7em", verticalAlign: "middle" }}>{user.name}</span>
             </div>
           </div>
-          <div style={{ float: "right", height: "100%", display: "flex", alignItems: "center" }}>
-            <RaisedButton label="Message" onClick={onSendMessage} style={{ marginRight: "1.5em" }} />
+          <div style={{ float: "right", height: "100%", display: "flex", alignItems: "flex-end" }}>
+            <RaisedButton label="Message" onClick={onSendMessage} style={{ marginRight: "0.7em" }} />
             <RaisedButton label={followLabel} onClick={onFollowClick} primary />
           </div>
         </div>
