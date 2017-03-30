c049ff2c4713abf4d498afa6cab47fd92afe5317
Cihan <cihannisan@gmail.com>
2017-03-30 07:49:56 -0700

    Add card popups

--- src/components/Markers.jsx
+++ src/components/Markers.jsx
@@ -1,21 +1,29 @@
 import React, { PropTypes } from "react";
 import autoBind from "react-autobind";
 import MapsPlace from "material-ui/svg-icons/maps/place"; 
-import { red500, orange500 } from 'material-ui/styles/colors';
+import { red500 } from "material-ui/styles/colors";
+import EventListItem from './EventList/EventListItem';
+import muiThemeable from 'material-ui/styles/muiThemeable';
 
-export default class Markers extends React.Component {
+class Markers extends React.Component {
 
   static propTypes = {
-    isHovered: PropTypes.bool
+    isHovered: PropTypes.bool,
+    event: PropTypes.object,
+    muiTheme: PropTypes.object,
+    $dimensionKey: PropTypes.string
   };
 
   constructor() {
     super();
     autoBind(this);
 
+    this.card = '';
+
     this.state = {
-      isHovered: false
-    }
+      isHovered: false,
+      isPopupOpen: false,
+    };
   }
 
   mouseEnter() {
@@ -26,15 +34,64 @@ export default class Markers extends React.Component {
     this.setState({ isHovered: false });
   }
 
+  onMarkerClick () {
+      this.card = 
+      <div style={{zIndex: 9999999999999}}>
+        <EventListItem           
+          eventUid={this.props.$dimensionKey}
+          event={this.props.event}
+          muiTheme={this.props.muiTheme}
+          popUp={true}
+        />
+      </div>;
+      this.setState({isPopupOpen: !this.state.isPopupOpen});
+  }
+
   render() {
     const markerStyle = {
-      smallMarker: {width:55, height:55, transform:'scale(0.6,0.6)', transformOrigin: '28px 55px'},
-      bigMarker: {width:55, height:55, transform:'scale(1,1)', transformOrigin: '28px 55px'}
+      smallMarker: { 
+        width:55, 
+        height:55, 
+        transform:'scale(0.6,0.6)', 
+        transformOrigin: '28px 55px',
+        zIndex: 0
+      },
+      bigMarker: {
+        width:55, 
+        height:55, 
+        transform:'scale(1,1)', 
+        transformOrigin: '28px 55px',
+        zIndex: 0
+      }
     };
+
     if(this.state.isHovered) {
-      return (<MapsPlace style={markerStyle.bigMarker} color={red500} hoverColor={orange500} onMouseOver={this.mouseEnter} onMouseOut={this.mouseLeave} />);
+      return ( 
+        <div>
+          <MapsPlace
+            style={markerStyle.bigMarker} 
+            color={red500} 
+            onMouseOver={this.mouseEnter} 
+            onMouseOut={this.mouseLeave} 
+            onClick={this.onMarkerClick}
+          />
+          {this.state.isPopupOpen ? this.card : <div></div> }
+        </div>
+      );
     } else {
-      return (<MapsPlace style={markerStyle.smallMarker} color={red500} hoverColor={orange500} onMouseOver={this.mouseEnter} onMouseOut={this.mouseLeave} />);
+      return ( 
+        <div>
+          <MapsPlace
+            style={markerStyle.smallMarker} 
+            color={red500} 
+            onMouseOver={this.mouseEnter} 
+            onMouseOut={this.mouseLeave} 
+            onClick={this.onMarkerClick}
+          />
+          {this.state.isPopupOpen ? this.card : <div></div> }
+        </div>
+      );
     }
   }
 }
+export default muiThemeable()(Markers);
