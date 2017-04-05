b7c060cdd7fbe492797f7c700dd304309228c7d7
mafergus <matthew.n.ferguson@gmail.com>
2017-04-05 11:15:11 -0700

    Onboarding WIP

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,8 +1,12 @@
 export const erfaraBlack = "#424242";
 export const darkGray = "#9E9E9E";
 export const minGray = "#EEEEEE";
-export const oneDark = "rgba(0, 0, 0, 0.87)";
-export const twiDark = "rgba(0, 0, 0, 0.54)";
-export const threeDark = "rgba(0, 0, 0, 0.38)";
-export const fourDark = "rgba(0, 0, 0, 0.12)";
-export const fiveDark = "rgba(0, 0, 0, 0.07)";
+export const darkOne = "rgba(0, 0, 0, 0.87)";
+export const darkTwo = "rgba(0, 0, 0, 0.54)";
+export const darkThree = "rgba(0, 0, 0, 0.38)";
+export const darkFour = "rgba(0, 0, 0, 0.12)";
+export const darkFive = "rgba(0, 0, 0, 0.07)";
+export const lightOne = "rgba(255, 255, 255, 1)";
+export const lightTwo = "rgba(255, 255, 255, 0.7)";
+export const lightThree = "rgba(255, 255, 255, 0.5)";
+export const lightFour = "rgba(255, 255, 255, 0.12)";
