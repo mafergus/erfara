ea23181f0989be2e285e325b2b5a4df3e08fa6f8
mafergus <matthew.n.ferguson@gmail.com>
2017-04-12 22:35:05 -0700

    Clear redux state when logging out

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -8,7 +8,7 @@ import { categoriesReducer } from "reducers/categoriesReducer";
 import { imagesReducer } from "reducers/imagesReducer";
 import { conversationsReducer } from "reducers/conversationsReducer";
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   authedUser: authedUserReducer,
   categories: categoriesReducer,
   conversations: conversationsReducer,
@@ -18,5 +18,13 @@ const rootReducer = combineReducers({
   browser: responsiveStateReducer,
   users: usersReducer,
 });
+
+ const rootReducer = (state, action) => {
+  if (action.type === "SIGN_OUT_USER") {
+    state = undefined
+  }
+
+  return appReducer(state, action)
+}
 
 export default rootReducer;
