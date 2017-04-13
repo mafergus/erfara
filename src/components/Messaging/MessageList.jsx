1a51ebc07f1875bd57c261dca8cbb71e8f90664f
mafergus <matthew.n.ferguson@gmail.com>
2017-04-13 15:47:55 -0700

    Make message list responsive and scroll to bottom on load

--- src/components/Messaging/MessageList.jsx
+++ src/components/Messaging/MessageList.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes } from "react";
+import ReactDOM from "react-dom";
 import { connect } from "react-redux";
 import autoBind from "react-autobind";
 import MessageListItem from "components/Messaging/MessageListItem";
@@ -6,6 +7,7 @@ import MessageListItem from "components/Messaging/MessageListItem";
 function mapStateToProps(state) {
   return {
     authedUser: state.authedUser,
+    isMobile: state.browser.is.extraSmall,
   };
 }
 
@@ -13,6 +15,7 @@ export class MessageList extends React.Component {
 
   static propTypes = {
     authedUser: PropTypes.object,
+    isMobile: PropTypes.bool.isRequired,
     messages: PropTypes.array.isRequired,
     style: PropTypes.object,
   };
@@ -26,8 +29,21 @@ export class MessageList extends React.Component {
     this.refs.list.scrollTop = this.refs.list.scrollHeight;
   }
 
+  scrollToBottom() {
+    const node = ReactDOM.findDOMNode(this.messagesEnd);
+    node.scrollIntoView({behavior: "smooth"});
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
   render() {
-    const { messages, style, authedUser } = this.props;
+    const { messages, style, authedUser, isMobile } = this.props;
     const STYLE = {
       ...style,
       listStyle: "none",
@@ -37,8 +53,14 @@ export class MessageList extends React.Component {
     };
     return <ul style={STYLE} ref="list">
       {messages.map((item, index) => {
-        return <MessageListItem key={item.date} message={item} isMine={authedUser.uid === item.from} />;
+        return <MessageListItem
+          key={item.date}
+          message={item}
+          isMine={authedUser.uid === item.from}
+          isMobile={isMobile}
+        />;
       })}
+      <div ref={el => { this.messagesEnd = el; }} />
     </ul>;
   }
 }
