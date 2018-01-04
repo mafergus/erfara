b801b5d0566e91ed98dd86f12cce27573deb3374
mafergus <matthew.n.ferguson@gmail.com>
2018-01-03 17:06:43 -0800

    Get the user's email from Facebook manually through the graph API

--- src/components/auth/AuthModal.jsx
+++ src/components/auth/AuthModal.jsx
@@ -25,23 +25,30 @@ export default class AuthModal extends React.Component {
       email: user.email,
       photo: user.photoURL,
     };
-    console.log("LE FIRST RESULT: ", result);
-    fetch(`https://graph.facebook.com/v2.11/me?fields=id%2Cname%2Cemail&access_token=${credential.accessToken}`).then(response => { 
-      if (response && response.ok) {
-        return response.json();
+
+    FB.api(
+      '/me',
+      'GET',
+      { "fields": "id,name,email" },
+      function(response) {
+        if (!response || response.error) {
+          alert("Couldn't get FB info");
+        } else {
+          userData = { ...userData, email: response.email};
+
+          checkUserExists(user.uid)
+          .then(() => getPhoto(), user => {
+            store.dispatch({ type: "ADD_AUTHED_USER_SUCCESS", user });
+            firebase.onAuthSuccess(user.uid);
+          })
+          .then(blob => uploadFile(blob))
+          .then(url => {
+            userData.coverPhoto = url;
+            store.dispatch(addUser(userData));
+          });
+        }
       }
-    })
-    .then(json => { userData = { ...userData, email: json.email }; console.log("email: ", json.email); })
-    .then(() => checkUserExists(user.uid))
-    .then(() => getPhoto(), user => {
-      store.dispatch({ type: "ADD_AUTHED_USER_SUCCESS", user });
-      firebase.onAuthSuccess(user.uid);
-    })
-    .then(blob => uploadFile(blob))
-    .then(url => {
-      userData.coverPhoto = url;
-      store.dispatch(addUser(userData));
-    });
+    );
   }
 
   constructor() {
@@ -50,52 +57,70 @@ export default class AuthModal extends React.Component {
   }
 
   componentDidMount() {
-    console.log("SHOULD GET FB GRAPH API CONNECTED");
-
     window.fbAsyncInit = function() {
       window.FB.init({
-        appId            : "1686372394915080",
-        autoLogAppEvents : true,
-        xfbml            : true,
-        version          : "v2.11"
+        appId      : "1686372394915080",
+        cookie     : true,  // enable cookies to allow the server to access
+                          // the session
+        xfbml      : true,  // parse social plugins on this page
+        version    : "v2.1" // use version 2.1
       });
-    };
-
-    // window.FB.getLoginStatus(function(response) {
-    //   this.statusChangeCallback(response);
-    // }.bind(this));
-
-    (function(d, s, id){
-       var js, fjs = d.getElementsByTagName(s)[0];
-       if (d.getElementById(id)) {return;}
-       js = d.createElement(s); js.id = id;
-       js.src = "https://connect.facebook.net/en_US/sdk.js";
-       fjs.parentNode.insertBefore(js, fjs);
-     }(document, 'script', 'facebook-jssdk'));
+  
+      // Now that we've initialized the JavaScript SDK, we call
+      // FB.getLoginStatus().  This function gets the state of the
+      // person visiting this page and can return one of three states to
+      // the callback you provide.  They can be:
+      //
+      // 1. Logged into your app ('connected')
+      // 2. Logged into Facebook, but not your app ('not_authorized')
+      // 3. Not logged into Facebook and can't tell if they are logged into
+      //    your app or not.
+      //
+      // These three cases are handled in the callback function.
+      window.FB.getLoginStatus(function(response) {
+        this.statusChangeCallback(response);
+      }.bind(this));
+    }.bind(this);
+    
+    // Load the SDK asynchronously
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
   }
 
   statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
+    // console.log('statusChangeCallback');
+    // console.log(response);
     // The response object is returned with a status field that lets the
     // app know the current login status of the person.
     // Full docs on the response object can be found in the documentation
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
-      // Logged into your app and Facebook.
       // this.testAPI();
-      console.log("CONNECTED");
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
-      // document.getElementById('status').innerHTML = 'Please log ' +
-        // 'into this app.';
     } else {
       // The person is not logged into Facebook, so we're not sure if
       // they are logged into this app or not.
-      // document.getElementById('status').innerHTML = 'Please log ' +
-      // 'into Facebook.';
     }
   }
+  
+  checkLoginState() {
+    window.FB.getLoginStatus(function(response) {
+      this.statusChangeCallback(response);
+    }.bind(this));
+  }
+
+  // testAPI() {
+  //   console.log('Welcome!  Fetching your information.... ');
+  //   window.FB.api('/me', function(response) {
+  //     console.log('Successful login for: ' + response.name);
+  //   });
+  // }
 
   handleSignUpFacebook() {
     this.props.handleClose();
